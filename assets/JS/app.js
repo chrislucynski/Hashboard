6e3fa79209bc5a5b0ada91ae4d5bd09fc0b6f064
Chris Lucynski <chris.lucynski@gmail.com>
2020-01-30 13:09:38 -0700

    created dashboard, whiched around directories

---
+++ assets/JS/app.js
@@ -0,0 +1,65 @@
+// Diagrams
+
+$('#diagrams-btn').click(function() {
+    $('.diagrams').toggleClass('hide')
+})
+
+$('#terpenes').click(function() {
+    $('#terpenes-img').toggleClass('hide')
+})
+
+$('#distillation').click(function() {
+    $('#distillation-img').toggleClass('hide')
+})
+
+$('#cannabinoids').click(function() {
+    $('#cannabinoids-img').toggleClass('hide')
+})
+$('#hemp').click(function() {
+    $('#hemp-img').toggleClass('hide')
+})
+
+
+// Converstion Table
+$('#conversion-table-btn').click(function() {
+    $('.conversion-tables').toggleClass('hide')
+})
+
+$('#gram-btn').click(function(event) {
+    $('#gram-data').toggleClass('hide')
+})
+
+$('#ounce-btn').click(function() {
+    $('#ounce-data').toggleClass('hide')
+})
+
+$('#temp-btn').click(function() {
+    $('#temp-data').toggleClass('hide')
+})
+
+
+// Recipes
+$('#recipes-btn').click(function() {
+    $('.recipes').toggleClass('hide')
+})
+
+$('#butter-btn').click(function() {
+    $('#butter').toggleClass('hide')
+})
+
+$('#oil-btn').click(function() {
+    $('#oil').toggleClass('hide')
+})
+
+$('#tincture-btn').click(function() {
+    $('#tincture').toggleClass('hide')
+})
+
+$('#gummies-btn').click(function() {
+    $('#gummies').toggleClass('hide')
+})
+
+// Dispensaries
+$('#dispensaries-btn').click(function(){
+    $('.dispensaries').toggleClass('hide')
+})
